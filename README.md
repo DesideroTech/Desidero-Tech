77757ab34db95da8b23137b9c1808e346281122e
DesideroTech <desiderotech@gmail.com>
2021-01-15 01:39:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-sgmydq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-sgmydq)
---
+++ pages/Calculator/Calculator.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-Calculator',
+  templateUrl: 'Calculator.html'
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
